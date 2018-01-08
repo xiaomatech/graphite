85d47638dc3ee86b835986812ef4b4b499842542
xianhua <xianhua@meizu.com>
2018-01-08 12:36:58 +0800

    'dashboard'

--- install_kenshin.sh
+++ install_kenshin.sh
@@ -16,7 +16,9 @@ mkdir -p /etc/kenshin /data/kenshin/{data,link,log} /var/run/kenshin
 
 cp conf/storage-schemas.conf.example /etc/kenshin/storage-schemas.conf
 
-rurouni_conf_base='''
+SERVER_IP=`/sbin/ifconfig  | grep 'inet'| grep -v '127.0.0.1' |head -n1 |tr -s ' '|cut -d ' ' -f3 | cut -d: -f2`
+
+rurouni_conf_base='
 \n[cache]
 \n# Configure rurouni-cache directories.
 
@@ -26,16 +28,16 @@ rurouni_conf_base='''
 \nLOG_DIR         = /data/kenshin/log
 \nPID_DIR         = /var/run/kenshin
 
-\nLINE_RECEIVER_INTERFACE = 0.0.0.0
-\nPICKLE_RECEIVER_INTERFACE = 0.0.0.0
-\nCACHE_QUERY_INTERFACE = 0.0.0.0
+\nLINE_RECEIVER_INTERFACE = '$SERVER_IP'
+\nPICKLE_RECEIVER_INTERFACE = '$SERVER_IP'
+\nCACHE_QUERY_INTERFACE = '$SERVER_IP'
 
 \nLOG_UPDATES = True
 \nMAX_CREATES_PER_MINUTE = 1000
 \nNUM_ALL_INSTANCE = '''$((`nproc`))'''
 
 \nDEFAULT_WAIT_TIME = 1
-\n\n'''
+\n\n'
 
 rurouni_conf_ext=''
 for instance in $(seq 0 $((`nproc`-1)))
@@ -66,22 +68,22 @@ graphite_header="
 \n  - kenshin_api.KenshinFinder
 \nkenshin:
 "
-graphite_footer="
+graphite_footer='
 \n\x20\x20memcached:
 \n\x20\x20\x20\x20expire_time: 20
 \n\x20\x20\x20\x20hosts:
-\n\x20\x20\x20\x20- 127.0.0.1:11211
+\n\x20\x20\x20\x20- '$SERVER_IP':11211
 \nfunctions:
 \n  - graphite_api.functions.SeriesFunctions
 \n  - graphite_api.functions.PieFunctions
 \n  - kenshin_api.functions.SeriesFunctions
-"
+'
 graphite_ext=""
 graphite_ext_dir=""
 for instance in $(seq 0 $((`nproc`-1)))
 do
     graphite_ext_dir=$graphite_ext_dir'\n\x20\x20- /data/kenshin/link/'$instance
-    graphite_ext=$graphite_ext'\n\x20\x20- 127.0.0.1:'$((4000+$instance))':'$instance
+    graphite_ext=$graphite_ext'\n\x20\x20- '$SERVER_IP':'$((4000+$instance))':'$instance
 done
 
 echo -ne $graphite_header'\n  directories:'$graphite_ext_dir'\n  carbonlink_hosts:'$graphite_ext'\n'$graphite_footer >/etc/graphite-api.yaml
@@ -91,7 +93,7 @@ echo -ne '''PORT="11211"
 USER="memcached"
 MAXCONN="65535"
 CACHESIZE="2048"
-OPTIONS=" -l 127.0.0.1 -t '''`nproc`'"'>/etc/sysconfig/memcached
+OPTIONS=" -l '$SERVER_IP' -t '''`nproc`'"'>/etc/sysconfig/memcached
 service memcached start
 
 echo -ne '[Unit]
@@ -99,7 +101,7 @@ Description=graphite-api socket
 
 [Socket]
 ListenStream=/run/graphite-api.sock
-ListenStream=0.0.0.0:8888
+ListenStream='$SERVER_IP':8888
 
 [Install]
 WantedBy=sockets.target'>/etc/systemd/system/graphite-api.socket
@@ -112,7 +114,7 @@ Requires=graphite-api.socket
 
 [Service]
 EnvironmentFile=-/etc/sysconfig/graphite-api
-ExecStart=/opt/kenshin/venv/bin/gunicorn -w '`nproc` 'graphite_api.app:app -b 0.0.0.0:8888
+ExecStart=/opt/kenshin/venv/bin/gunicorn -w '`nproc` 'graphite_api.app:app -b '$SERVER_IP':8888
 Restart=on-failure
 #User=graphite
 #Group=graphite
--- install_relay.sh
+++ install_relay.sh
@@ -5,25 +5,27 @@ rpm -ivh https://raw.githubusercontent.com/xiaomatech/graphite/master/carbon-c-r
 mkdir -p /var/log/carbon-relay /var/run/carbon-relay
 chown -R carbon-c-relay:carbon-c-relay /var/log/carbon-relay /var/run/carbon-relay
 
-echo -ne 'ARGS="-p 2999 -B 4096 -l /var/log/carbon-relay/carbon-relay.log -P /var/run/carbon-relay/carbon-relay.pid"'>/etc/sysconfig/carbon-c-relay
+
+SERVER_IP=`/sbin/ifconfig  | grep 'inet'| grep -v '127.0.0.1' |head -n1 |tr -s ' '|cut -d ' ' -f3 | cut -d: -f2`
+
+echo -ne 'ARGS="-p 2003 -B 4096 -l /var/log/carbon-relay/carbon-relay.log -P /var/run/carbon-relay/carbon-relay.pid"'>/etc/sysconfig/carbon-c-relay
 
 header='''
-cluster local_carbon
-\n  forward
+cluster online
+\n  jump_fnv1a_ch
 '''
 
 footer='''
 \n  ;
 \nmatch \x2A
-\n  send to local_carbon
-\n  stop
+\n  send to online
 \n  ;
 '''
 
 cluster_address=''
 for instance in $(seq 0 $((`nproc`-1)))
 do
-    cluster_address=$cluster_address'\n\x20\x20\x20\x20127.0.0.1:'$((2000+$instance))
+    cluster_address=$cluster_address'\n\x20\x20\x20\x20'$SERVER_IP':'$((2000+$instance))
 done
 
 echo -ne $header$cluster_address$footer>/etc/carbon-c-relay.conf
